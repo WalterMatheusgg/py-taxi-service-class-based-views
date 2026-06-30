86a74786654ed60127e42217acb1c75296ed30ff
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-06-30 17:49:26 +0000

    Implement class-based views for taxi service

--- taxi/views.py
+++ taxi/views.py
@@ -5,14 +5,11 @@ from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
-
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
-
     return render(request, "taxi/index.html", context=context)
 
 
