eaf44d8d2e8d10ec007f37a603b3b15dc2a1331c
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-06-30 18:02:23 +0000

    Implement class-based views for taxi service

--- taxi/views.py
+++ taxi/views.py
@@ -1,31 +1,60 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.template.backends import django
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
 
 def index(request):
+    """View function for the home page of the site."""
+
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
+
     return render(request, "taxi/index.html", context=context)
 
 
 class ManufacturerListView(ListView):
     model = Manufacturer
-    template_name = "taxi/manufacturer_list.html"
-    context_object_name = "manufacturer_list"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Manufacturer.objects.all().order_by("name")
 
 
 class CarListView(ListView):
     model = Car
-    template_name = "taxi/car_list.html"
-    context_object_name = "car_list"
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer").all()
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+    def get_queryset(self):
+        return (Car.objects.select_related("manufacturer")
+                .prefetch_related("drivers"))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["drivers"] = self.object.drivers.all()
+
+        return context
 
 
 class DriverListView(ListView):
     model = Driver
-    template_name = "taxi/driver_list.html"
-    context_object_name = "driver_list"
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars").all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cars"] = context["driver"].cars.all()
+        return context
