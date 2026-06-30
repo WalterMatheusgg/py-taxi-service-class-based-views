bc16ccf7e602d99728250c9ab2a6bd9cf39e8efe
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-06-30 17:32:52 +0000

    Implement class-based views for taxi service

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+
 from .views import index, ManufacturerListView
 
 app_name = "taxi"
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list",
+    ),
 ]
